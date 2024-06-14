c599440c37a91ecdf7b9976e02c8ebf1a88d1dc9
CrrtN30 <andi.akbar@student.unismuh.ac.id>
2024-06-14 16:21:22 +0800

    Text Input

--- App.js
+++ App.js
@@ -1,71 +1,35 @@
-import { Text, View, Image, ScrollView } from 'react-native';
+import { Text, View, Image, TextInput} from 'react-native';
 import React from 'react';
-import ButtonComponent from './Components/Buttons/button';
+
 
 const App = () => {
-  
+  const Inputan = ({nama, color})=>{
+    return(
+      <TextInput 
+      placeholder={`Masukan ${nama}`}
+      style={{
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 10,
+        width: 300,
+        height: 50,
+        marginVertical: 10,
+        padding: 10,
+        backgroundColor: 'gray',
+        color: color,
+      }}
+      />
+    )
+  }
   return (
     <View style={{
       flex: 1,
-      backgroundColor: '#000',
+      justifyContent: 'center',
+      alignItems: 'center',
     }}>
-      <Image
-        source={require('./assets/onepiece.png')}
-        style={{
-          width: '100%',
-          height: '100%',
-          position: 'absolute',
-          opacity: 0.5,
-          zIndex: -1,
-        }}
-        resizeMode="cover"
-      />
-      <ScrollView contentContainerStyle={{
-        flexGrow: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 20,
-      }}>
-        <Image 
-          source={require('./assets/one-piece-logo-75.png')} 
-          style={{
-            width: 200,
-            height: 200,
-            marginTop: 50,
-            opacity: 1,
-            zIndex: 1,
-          }}
-        />
-        <Text style={{
-          color: 'white',
-          fontSize: 28,
-          fontWeight: '300',
-          textAlign: 'center',
-          marginTop: 20,
-        }}>
-          Nonton One Piece Tiap Hari Minggu
-        </Text>
-        <Text style={{
-          color: 'white',
-          fontSize: 18,
-          textAlign: 'center',
-          marginTop: 20,
-          marginBottom: 20,
-        }}>
-          Ayolah Nakama kita nonton One Piece.
-        </Text>
-        <View style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          marginBottom: 50,
-        }}>
-
-          <ButtonComponent title="Sign In" color="#007bff" />
-          <ButtonComponent title="Sign Up" color="#06D84F" />
-          
-        </View>
-      </ScrollView>
+        <Inputan nama="Email" color="white"/>
+        <Inputan nama="Username" color="white"/>
+        <Inputan nama="Password" color="black"/>
     </View>
   );
 
